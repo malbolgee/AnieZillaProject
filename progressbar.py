914fd664508de124a1c4535601b38da061488815
Victor Hugo <victor.oliveiragomes@outlook.com>
2019-12-28 12:42:33 -0400

    minor change regardless ftp issue
    - Fixed an error when transferring large files over ftp that causes the upload to hang forever

--- progressbar.py
+++ progressbar.py
@@ -3,9 +3,11 @@ from datetime import datetime
 
 class progressBar(Frame):
 
-    def __init__(self, parent, controller, maxbytes, start_time):
+    def __init__(self, parent, controller, maxbytes, start_time, ftp):
         Frame.__init__(self, parent)
 
+        self.ftp = ftp
+        self.timeBegin = 0
         self.parent = parent
         self.controller = controller
         self.sizeWritten = 0
@@ -17,9 +19,14 @@ class progressBar(Frame):
         self.progress.pack(pady = 5)
 
     def updateProgress(self, block):
-        self.sizeWritten += 8192
+        self.sizeWritten += 20000
         percenteComplete = round((self.sizeWritten / self.maxbytes) * 100)
         end_time = datetime.now()
+
+        if (end_time - self.timeBegin).total_seconds() > 10:
+            self.timeBegin = datetime.now()
+            self.ftp.voidcmd('NOOP')
+
         kbytespersec = (self.sizeWritten / 1024) / (end_time - self.start_time).total_seconds()
         self.controller.percentageLabel['text'] = '{}%      -      {:.0f} Kbps'.format(percenteComplete, kbytespersec)
         self.progress['value'] = percenteComplete
