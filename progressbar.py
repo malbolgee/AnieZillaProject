750e2873465c89d30a6b0ea666cf62ec604f07b6
Victor Hugo <victor.oliveiragomes@outlook.com>
2020-02-01 13:37:54 -0400

    bug fixes
    - fixed a bug concerning the issue #2

--- progressbar.py
+++ progressbar.py
@@ -3,19 +3,20 @@ from datetime import datetime
 
 class progressBar(Frame):
 
-    def __init__(self, parent, controller, maxbytes, start_time, ftp):
+    def __init__(self, parent, controller, maxbytes, ftp):
         Frame.__init__(self, parent)
 
         self.ftp = ftp
+        self.rest = False
         self.stop = False
         self.pause = False
-        self.rest = False
-        self.sizeWrittenRest = 0
-        self.timeBegin = 0
-        self.controller = controller
+        self.NewSizeWritten = 0
         self.sizeWritten = 0
-        self.start_time = start_time
+        self.sizeWrittenRest = 0
         self.maxbytes = maxbytes
+        self.controller = controller
+        self.timeBegin = datetime.now()
+        self.start_time = datetime.now()
         self.progress = ttk.Progressbar(parent, orient = 'horizontal', length = 293, mode = 'determinate')
         self.progress['value'] = 0
         self.progress['maximum'] = 100
@@ -23,24 +24,25 @@ class progressBar(Frame):
 
     def updateProgress(self, block):
 
-        self.sizeWritten += 20000
-        self.sizeWrittenRest += 20000
+        self.sizeWritten += 24576
+        self.sizeWrittenRest += 24576
 
-        percenteComplete = round((self.sizeWrittenRest / self.maxbytes) * 100)
         end_time = datetime.now()
 
         if (end_time - self.timeBegin).total_seconds() > 10:
             self.timeBegin = datetime.now()
             self.ftp.voidcmd('NOOP')
-
-        kbytespersec = (self.sizeWritten / 1024) / (end_time - self.start_time).total_seconds()
         
         if self.rest == False:
+            kbytespersec = (self.sizeWritten / 1024) / (end_time - self.start_time).total_seconds()
             eta = self.getETA(self.maxbytes - self.sizeWritten, kbytespersec)
         else:
+            self.NewSizeWritten += 24576
+            kbytespersec = (self.NewSizeWritten / 1024) / (end_time - self.start_time).total_seconds()
             eta = self.getETA(self.maxbytes - self.sizeWrittenRest, kbytespersec)
 
         self.controller.footLabel['text'] = '{}'.format(eta)
+        percenteComplete = round((self.sizeWrittenRest / self.maxbytes) * 100)
         self.controller.percentageLabel['text'] = '{}% - {:.0f} Kbps'.format(percenteComplete, kbytespersec)
         self.progress['value'] = percenteComplete
 
