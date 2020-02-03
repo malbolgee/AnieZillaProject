f03d604050b7adc7284d9486d62e75bb2b91c0dc
Victor Hugo <victor.oliveiragomes@outlook.com>
2020-02-02 21:58:43 -0400

    added some constants

--- constants.py
+++ constants.py
@@ -5,6 +5,10 @@ USER = 'aniedb'
 PASSWORD = 'starred1234'
 DBNAME = 'aniedb'
 
+FTP_SERVER = 'ftp.anieclipse.tk'
+FTP_USER = 'anieclipse3'
+FTP_PASSWORD = 'StarBugs#029'
+
 VIEWS = 0
 
 INSERT_QUERY = "INSERT INTO episodios (idUsuario, idObra, nome, numero, duracao, thumb, nomeArquivo, qualidadeMax, temporada, dataPostagem, views) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
@@ -13,7 +17,7 @@ SELECT_COUNT_QUERY = "SELECT COUNT(id) FROM episodios WHERE idObra = %s"
 SELECT_NUM_EPISODES_QUERY = "SELECT numeroEpisodios FROM obras WHERE id = %s"
 IS_REPEATED_QUERY = "SELECT * FROM episodios WHERE idObra = %s and numero = %s"
 ANIME_LIST_QUERY = "SELECT id, nome, diretorio FROM obras INNER JOIN animes ON id = idObra WHERE upado = 0"
-USER_LIST_QUERY = "SELECT id, login, senha FROM usuarios WHERE up = 1"
+USER_LIST_QUERY = "SELECT id, senha, up FROM usuarios WHERE BINARY login = %s"
 
 def timestamp():
     """ Returns a timestamp for the current date and time. """
@@ -29,8 +33,16 @@ STOPED = 4
 PAUSED = 5
 QUEUED = 6
 
-UPDATE_UPLOAD_LIST_COLOR = ['green', 'purple', 'red', 'orange', 'red', 'blue']
+UPDATE_UPLOAD_LIST_COLOR = ['green', 'purple', 'red', 'orange', '#8b0000', 'blue']
 UPDATE_UPLOAD_LIST_STRING = [' -> Terminado.', ' -> Repetido, não upado.', ' -> Erro no upload.', ' -> Upando...', ' -> Parado.', ' -> Pausado']
 
 EMPTY_CFG = 0
 LESS_CFG = 1
+
+THUMB_REGEX = r'\bthumb-\d+[.]png\b'
+EPISODE_REGEX = r'\bepisodio-\d+[.]mp4\b'
+
+INVALID_LOGIN_LABEL_ARGS = {'text': 'Login inválido', 'fg': 'red', 'font': ('Calibri', 10, 'italic')}
+SHOW_LIST_BOX_ARGS = {'font' : ('Calibri Light', 10), 'relief' : 'flat', 'activestyle' : 'none', 'bd' : 0, 'fg' : '#ef3b37', 'selectbackground' : '#333333', 'selectforeground' : '#ef3b37', 'highlightthickness' : 0}
+FILE_LIST_BOX_ARGS = {'font' : ('Calibri Light', 9), 'relief' : 'flat', 'activestyle' : 'none', 'bd' : 0, 'fg' : '#ef3b37', 'selectbackground' : '#333333', 'selectforeground' : '#ef3b37', 'highlightthickness' : 0}
+UPLOAD_LIST_BOX_ARGS = {'font' : ('Calibri Light', 9), 'relief' : 'flat', 'activestyle' : 'none', 'bd' : 0, 'fg' : '#ef3b37', 'selectbackground' : '#333333', 'selectforeground' : '#ef3b37', 'highlightthickness' : 0}
